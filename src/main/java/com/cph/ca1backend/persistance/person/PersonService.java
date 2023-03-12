03bc34a2eb39c8d8b819ef441a8f24770d2f82c2
AbdullahMarwan <cph-ae165@cphbusiness.dk>
2023-03-12 03:04:56 +0100

    More cleanup.

--- src/main/java/com/cph/ca1backend/persistance/person/PersonService.java
+++ src/main/java/com/cph/ca1backend/persistance/person/PersonService.java
@@ -22,4 +22,30 @@ public class PersonService {
     public void deletePerson(Integer id) {
         personRepository.deleteById(id);
     }
+
+    /*
+     public PersonMapper(PersistenceManager persistenceManager) {
+        this.persistenceManager = persistenceManager;
+    }
+
+    public Person getSpecificPerson(int personId) {
+        Logger.getLogger("web").log(Level.INFO, "");
+
+        return persistenceManager.getEntityManager().find(Person.class, personId);
+    }
+
+    public List<Person> getAllPeople() {
+        TypedQuery<Person> query = persistenceManager.getEntityManager().createNamedQuery("getAllPeople", Person.class);
+
+        return query.getResultList();
+    }
+
+    public void create(Person person) {
+        Logger.getLogger("web").log(Level.INFO, "");
+
+        persistenceManager.entityTransaction().begin();
+        persistenceManager.getEntityManager().persist((person));
+        persistenceManager.entityTransaction().commit();
+    }
+     */
 }
